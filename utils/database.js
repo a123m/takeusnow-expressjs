d0ffca0c03af43ec08f78405c89948ad08e636ce
aman <aman.chhabra@sourcefuse.com>
2020-10-17 12:44:23 +0530

    dev

--- utils/database.js
+++ utils/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   // host: "database-1.cisa2w1gawoz.ap-south-1.rds.amazonaws.com",
-  host: 'database-1.ccejucbp66ft.ap-south-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'snaplancing123',
+  host: 'localhost',
+  user: 'root',
+  password: 'asdfghjkl',
   database: 'SLDB',
   port: '3306',
   multipleStatements: true,
