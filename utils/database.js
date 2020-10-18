fa39e6211dbeac8f29442eec0670104cce5733fa
Kaushalendra Singh <kaushalendra@sourcesoftsolutions.com>
2020-10-18 17:53:28 +0530

    status update

--- utils/database.js
+++ utils/database.js
@@ -2,23 +2,23 @@ const mysql = require('mysql2');
 
 /* Live server DB details */
 
-// const pool = mysql.createPool({
-//   host: 'sldb.ck8gk1zfzscd.ap-south-1.rds.amazonaws.com',
-//   user: 'root',
-//   password: 'takeusnow',
-//   database: 'SLDB',
-//   port: '3306',
-//   multipleStatements: true,
-// });
-
-/* Local DB credentials */
-
 const pool = mysql.createPool({
-  user: 'kaushal',
-  password: '11223344',
+  host: 'sldb.ck8gk1zfzscd.ap-south-1.rds.amazonaws.com',
+  user: 'root',
+  password: 'takeusnow',
   database: 'SLDB',
   port: '3306',
   multipleStatements: true,
 });
+
+/* Local DB credentials */
+
+// const pool = mysql.createPool({
+//   user: 'kaushal',
+//   password: '11223344',
+//   database: 'SLDB',
+//   port: '3306',
+//   multipleStatements: true,
+// });
 
 module.exports = pool.promise();
